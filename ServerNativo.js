f1b3813b5b98a374fbd338e423ba9316d9eb8689
EduMarsola <eduimarsola@gmail.com>
2025-01-19 21:51:29 -0300

    19/01/2025-2

---
+++ ServerNativo.js
@@ -0,0 +1,15 @@
+import { createServer, Server } from 'node:http' //node:crypto -> hash
+
+
+const server = createServer((request, response) => { 
+    console.log("ola")      
+    response.write("oi")
+    return response.end()
+})
+
+server.listen(3333)
+
+
+
+
+
