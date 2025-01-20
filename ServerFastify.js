50644552d1d8176d7c120da01f813ef1a0beb451
EduMarsola <eduimarsola@gmail.com>
2025-01-20 20:06:27 -0300

    02/01/2025

--- ServerFastify.js
+++ ServerFastify.js
@@ -5,22 +5,41 @@ import { DatabaseMemory } from './database-memory.js'
 const database = new DatabaseMemory()
 
 const server = fastify()
-server.listen({port: 3333})
 server.get('/', () => {return 'Hello, World raiz'}) //quando o cliente acessar a pasta raiz abre nessa função
 server.get('/1', () => {return 'Hello, World 1'})
 server.get('/2', () => {return 'Hello, World 2'})
-server.post('/video', () => {
+    
+server.post('/videos', (request, reply) => {
+    const {tittle, description, duration} = request.body
     database.create({
-        tittle : 'titulo',
-        description : "descricao",
-        duration : 180
+        tittle : tittle,
+        description : description,
+        duration : duration, //ou somente tittle, description, duration (short sintax)
     })
-    console.log(database.list())
-})
-server.get('/video', () => {})
-server.put('/video/:id', () => {})
-server.delete('/video/:id', () => {})
 
+    return reply.status(201).send() //algo foi criado
+})
+
+server.get('/videos', (request, reply) => {
+    const videos = database.list()
+    return videos
+})
+
+server.put('/videos/:id', (request, reply) => {
+    const videoID = request.params.id
+    const {tittle, description, duration} = request.body
+    database.update(videoID, {
+        title,
+        description,
+        duration
+    })
+    return reply.statusCode(204).send() //sucesso sem conteúdo na resposta
+})
+
+
+server.delete('/videos/:id', () => {})
+
+server.listen({port: 3333})
 
 
 
--- database-memory.js
+++ database-memory.js
@@ -3,14 +3,29 @@ import { randomUUID } from 'node:crypto'
 
 export class DatabaseMemory{
     #videos = new Map()
+    list()
+    {
+        return Array.from(this.#videos.entries()).map((videoArray) =>{
+            const id = videoArray[0]
+            const data = videoArray[1]
+            return {
+                id,
+                ...data, //spread populator do JS
+            } 
+        }) //encapsula o iterator para array
+    }
     create(video)
     {
-        const id =randomUUID.
-        this.#videos.set(video)
+        const videoId = randomUUID()
+        this.#videos.set(videoId, video)
     }
     update(id, video)
     {
-        this.#videos
+        this.#videos.set(id, video)
+    }
+    delete(id)
+    {
+        this.#videos.delete(id)
     }
 
 }
